b3f4d2f148d8bba9f25e1925079f99ef2a34726e
xiongrw <xiongrw0701@163.com>
2025-11-04 15:32:40 +0800

    Initial commit

---
+++ src/main/java/com/houseleasing/houseleasingmanagementsystem/HouseLeasingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.houseleasing.houseleasingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseLeasingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseLeasingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/houseleasing/houseleasingmanagementsystem/HouseLeasingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houseleasing.houseleasingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseLeasingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
